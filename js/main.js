7a21a2b7338fc8e9e298b00f008b26ce78758b72
fedrixt <fedrixt@gmail.com>
2018-09-27 03:21:21 +0300

    next step - carousel created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-
-
-
-
-
-
-
-
-
-
-
+(function () {
+  var callOwlCarousel = function() {
+    $(".owl-carousel").owlCarousel({
+      items:1,
+      loop:true,
+      center: true,
+      stagePadding: -10,
+      // nav: true,
+      // autoplay: true,
+      autoplayTimeout: 5000,
+      autoplaySpeed: 1000,
+      autoplayHoverPause: false,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+    });
+  };
+  // call functions
+  $(function () {
+    callOwlCarousel();
+  });
+}());
